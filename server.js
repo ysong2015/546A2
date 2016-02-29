8823b73e0756b16b8577daf3fb2e2659f368df51
Yumeng Song <ymeng.song@gmail.com>
2016-02-29 12:49:54 -0500

    All feature for A2.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // We first require our express package
 var express = require('express');
 var bodyParser = require('body-parser');
-var myData = require('./data.js');
+var toDoTasks = require('./toDoEntries.js');
 
 // This package exports the function to create an express instance:
 var app = express();
@@ -18,16 +18,106 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 // it will check the 'static' folder for matching files 
 app.use('/assets', express.static('static'));
 
-// Setup your routes here!
+app.use(function (req, res, next) {
+    if (req.body && req.body._method) {
+        req.method = req.body._method;
+        delete req.body._method;
+    }
 
-app.get("/home", function (request, response) {
-    response.render("pages/home", { pageTitle: "Welcome Home" });
+    // let the next middleware run:
+    next();
 });
+
+// Setup your routes here!
 
 app.get("/", function (request, response) { 
     // We have to pass a second parameter to specify the root directory
     // __dirname is a global variable representing the file directory you are currently in
     response.sendFile("./pages/index.html", { root: __dirname });
+});
+
+app.get("/api/todo", function (request, response) {
+    var displayType = "",
+        toDOEntrytoShow = [];
+    displayType = "All Tasks";
+    toDOEntrytoShow = toDoTasks.getAllToDoTasks();
+    console.log(toDOEntrytoShow);
+    response.render('pages/home', { task: toDOEntrytoShow, type: displayType, pageTitle: "To-Do List" });
+});
+
+app.get("/api/todo/open", function (request, response) {
+    var displayType = "",
+        toDOEntrytoShow = [];
+    displayType = "Open Tasks";
+    toDOEntrytoShow = toDoTasks.getOpenToDoTasks();
+    response.render('pages/home', { task: toDOEntrytoShow, type: displayType, pageTitle: "To-Do List" });
+});
+
+app.get("/api/todo/completed", function (request, response) {
+    var displayType = "",
+        toDOEntrytoShow = [];
+    displayType = "Completed Tasks";
+    toDOEntrytoShow = toDoTasks.getCompletedToDoTasks();
+    response.render('pages/home', { task: toDOEntrytoShow, type: displayType, pageTitle: "To-Do List" });
+});
+
+app.get("/api/todo/:id", function (request, response) {
+    try {
+        var task = toDoTasks.getToDoTask(request.params.id);
+        // we caught an exception! Let's show an error page!
+        response.render('pages/task', { task: task, pageTitle: task.taskTitle });
+    } catch (error) {
+        // we caught an exception! Let's show an error page!
+        response.render('pages/error', { errorType: "Issue loading question!", errorCode: error.code, errorMessage: error.message });
+    }
+});
+
+// Create a new task
+app.post("/api/todo", function (request, response) {
+    try {
+        console.log(request.body);
+        var task = toDoTasks.addToDoTask(request.body.author, request.body.taskTitle, request.body.taskDescription);
+        response.render('pages/task', { task: task, pageTitle: task.taskTitle });
+    } catch (error) {
+        // we caught an exception! Let's show an error page!
+        response.render('pages/error', { errorType: "Issue creating question!", errorCode: error.code, errorMessage: error.message });
+    }
+});
+
+// Update a task
+app.put("/api/todo/:id", function (request, response) {
+    try {
+        var task = toDoTasks.updateToDoTask(request.params.id, request.body.taskTitle, request.body.taskDescription, request.body.status);
+        // we caught an exception! Let's show an error page!
+        response.render('pages/task', { task: task, pageTitle: task.title });
+    } catch (error) {
+        // we caught an exception! Let's show an error page!
+        response.render('pages/error', { errorType: "Issue updating question!", errorCode: error.code, errorMessage: error.message });
+    }
+})
+
+// Add a note for the task
+app.post("/api/todo/:id/notes", function (request, response) {
+    try {
+        var task = toDoTasks.addNotes(request.params.id, request.body.taskNotes);
+        response.render('pages/task', { task: task, pageTitle: task.taskTitle });
+    } catch (error) {
+        // we caught an exception! Let's show an error page!
+        response.render('pages/error', { errorType: "Cannot answer question!", errorCode: error.code, errorMessage: error.message });
+    }
+});
+
+app.delete("/api/todo/:id", function (request, response) {
+    try {
+        console.log("====" + request.params.id);
+        toDoTasks.deleteToDoTask(request.params.id);
+        var toDOEntrytoShow = toDoTasks.getAllToDoTasks();
+        console.log(toDOEntrytoShow);
+        response.render('pages/home', { task: toDOEntrytoShow, type: "All Tasks", pageTitle: "To-Do List" });
+    } catch (error) {
+        // we caught an exception! Let's show an error page!
+        response.render('pages/error', { errorType: "Cannot answer question!", errorCode: error.code, errorMessage: error.message });
+    }
 });
 
 // We can now navigate to localhost:3000
--- toDoEntries.js
+++ toDoEntries.js
@@ -2,73 +2,75 @@ var exports = module.exports = {};
 
 var toDoList = [];
 
-var makeAnToDOEntry = function (author, taskTitle, taskDescription) {
+var makeAnToDOTask = function (author, taskTitle, taskDescription) {
     if (!author || !taskTitle || !taskDescription)
-        throw "You must provide valid information in the request body to create an entry.";
-    return { id: toDoList.length+1, author: author, 
-    taskTitle: taskTitle, taskDescription: taskDescription, taskNotes: [], status: "open" };
+        throw { code: 500, message: "You must provide valid information in the request body to create an entry." };
+    return { id: toDoList.length + 1, author: author, taskTitle: taskTitle, taskDescription: taskDescription, taskNotes: [], status: "open" };
 }
 
-exports.getAllToDoEntries= function () {
+exports.getAllToDoTasks = function () {
     return toDoList.slice(0);
 };
 
-exports.getOpenToDoEntries=function () {
+exports.getOpenToDoTasks = function () {
     return toDoList.filter(function (toDoEntry) {
-        return toDoEntry.status=="open";
+        return toDoEntry.status == "open";
     });
 }
 
-exports.getCompletedToDoEntries=function () {
+exports.getCompletedToDoTasks = function () {
     return toDoList.filter(function (toDoEntry) {
-        return toDoEntry.status=="completed";
+        return toDoEntry.status == "completed";
     });
 }
 
-exports.getToDoEntry = function(id) {
+exports.getToDoTask = function (id) {
     if (typeof id === "string") id = parseInt(id);
 
-    if (id !== 0 && !id) throw "Must provide ID";
+    if (id !== 0 && !id) throw { code: 500, message: "Must provide ID" };
 
     var toDoEntry = toDoList.filter(function (toDoEntry) {
         return toDoEntry.id === id;
     }).shift();
 
-    if (!toDoEntry) throw "An entry with the ID of"+id+"could not be found";
+    if (!toDoEntry) throw { code: 404, message: "An entry with the ID of" + " " + id + " " + "could not be found" };
 
     return toDoEntry;
 };
 
-exports.addToDoItem=function (author,taskTitle,taskDescription) {
-    var toDoEntry = exports.makeAnToDOEntry(author,taskTitle,taskDescription);
+exports.addToDoTask = function (author, taskTitle, taskDescription) {
+    var toDoEntry = makeAnToDOTask(author, taskTitle, taskDescription);
     toDoList.push(toDoEntry);
     return toDoEntry;
 };
 
-exports.updateToDoItem=function (id,taskTitle,taskDescription,status) {
-    if (!taskTitle&&!taskDescription&&!status) 
-    throw "You must provide valid information in the request body to create an entry.";
-    
-    var toDoEntry = exports.getToDoEntry(id);
-    toDoEntry.taskTitle=taskTitle;
-    toDoEntry.taskDescription=taskDescription;
-    toDoEntry.status=status;
-    
+exports.updateToDoTask = function (id, taskTitle, taskDescription, status) {
+    if (!taskTitle && !taskDescription && !status)
+        throw { code: 500, message: "You must provide valid information in the request body to create an entry." };
+
+    var toDoEntry = exports.getToDoTask(id);
+    toDoEntry.taskTitle = taskTitle;
+    toDoEntry.taskDescription = taskDescription;
+    toDoEntry.status = status;
+
     return toDoEntry;
 };
 
-exports.addNotes=function (id,note) {
-    if (!note) throw "You must provide valid information in the request body to create an entry.";
-    
-    var toDoEntry = exports.getToDoEntry(id);
-    toDoEntry.note=note;
-    
+exports.addNotes = function (id, note) {
+    if (!note) throw { code: 500, message: "You must provide valid information in the request body to create an entry." };
+
+    var toDoEntry = exports.getToDoTask(id);
+    toDoEntry.taskNotes.push(note);
+
     return toDoEntry;
 }
 
-exports.deleteToDoEntry=function (id) {
-    var toDoEntry=exports.getToDoEntry(id);
+exports.deleteToDoTask = function (id) {
+    var toDoEntry = exports.getToDoTask(id);
     var indexOfToDoEntry = toDoList.indexOf(toDoEntry);
-    
-    toDoList.splice(indexOfToDoEntry,1);
+
+    toDoList.splice(indexOfToDoEntry, 1);
 }
+
+var firstToDo = exports.addToDoTask("Yumeng", "task1", "This is the description for first task.");
+var secondToDo = exports.addToDoTask("Song", "task2", "This is the description for second task.");
